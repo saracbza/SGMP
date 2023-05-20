ee89ac35016f20dcadc196e2b938946abd44c745
saracbza <sasa.cb2002@gmail.com>
2023-05-20 00:27:39 -0300

    alteracoes para correcao de rotas

--- routes/ProdutoRoutes.js
+++ routes/ProdutoRoutes.js
@@ -3,8 +3,8 @@ import ProdutoController from '../controllers/ProdutoController.js'
 
 const ProdutoRouter = express.Router()
 
-ProdutoRouter.get('/produtos', ProdutoController.list)
-ProdutoRouter.post('/produto', ProdutoController.createProduto)
+ProdutoRouter.get('/', ProdutoController.list)
+ProdutoRouter.post('/', ProdutoController.createProduto)
 ProdutoRouter.get('/:cod', ProdutoController.getProdutoById)
 ProdutoRouter.put('/:cod', ProdutoController.updateProduto)
 ProdutoRouter.delete('/:cod', ProdutoController.destroyProduto)
