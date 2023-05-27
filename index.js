5ca6d6c6210d80c0b3fe7460f31f1d6b14cf0bd8
saracbza <sasa.cb2002@gmail.com>
2023-05-26 23:25:23 -0300

    atualizacao do index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import cors from "cors"
+import db from "./db.js"
 import ProdutoRouter from './routes/ProdutoRoutes.js'
 import MateriaRouter from './routes/MateriaRoutes.js'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/produtos', ProdutoRouter)
 app.use('/materias-prima', MateriaRouter)
-const port = 3000
+const port = process.env.PORT || 3000
+
 app.listen(port, () => {console.log(`API rodando na porta ${port}!`)})
