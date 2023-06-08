2f0b114c31daf481c4ebb11d43cbb45f01f0fd2e
saracbza <sasa.cb2002@gmail.com>
2023-06-08 01:10:48 -0300

    associação entre tabelas

--- models/index.js
+++
@@ -1,8 +0,0 @@
-import Produto_materiaPrima from "./Produto_materiaPrima.js"
-import Materia from "./Materia.js"
-import Produto from "./Produto.js"
-
-Materia.belongsToMany(Produto, { through: Produto_materiaPrima, as: "produtos" })
-Produto.belongsToMany(Materia, { through: Produto_materiaPrima, as: "materias" })
-
-export { Materia, Produto_materiaPrima, Produto }
